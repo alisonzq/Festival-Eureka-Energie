d0608d3a4179767f0f0627140d208c6ba5df888b
Alison <alisonquato@gmail.com>
2023-04-05 00:11:24 -0400

    code, anim button turn light on/off

--- Assets/TurnLightOn.cs
+++ Assets/TurnLightOn.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class TurnLightOn : MonoBehaviour
 {
-  public GameObject light;
+    public GameObject spotlight;
+    private Animator animator;
+    private int numPushed;
+    public GameObject checkAnim;
 
-  void OnTriggerEnter (Collider target) 
-  {
-    if( target.gameObject.tag.Equals("turnOn") == true )
+    void Start()
     {
-      //Écrit le code en dessous de cette ligne / Write code below this line
-        light.SetActive(true);
+        animator = GetComponent<Animator>();
     }
-  }
 
+    private void OnMouseUpAsButton()
+    {
+        Animator eolienneAnimator = checkAnim.GetComponent<Animator>();
+        if (eolienneAnimator.GetCurrentAnimatorStateInfo(0).IsName("eolienne"))
+        {
+            Debug.Log("There is electricity!");
+            numPushed++;
+            //Debug.Log(numPushed);
+            if (numPushed % 2 != 0)
+            {
+                //Écrit le code en dessous de cette ligne / Write code down below
+                animator.SetTrigger("pushed"); //soit ca
+                spotlight.SetActive(true); //ou ca
+            }
+            else
+            {
+                animator.SetTrigger("pushed");
+                spotlight.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.Log("no electricity!");
+        }
+
+    }
 }
